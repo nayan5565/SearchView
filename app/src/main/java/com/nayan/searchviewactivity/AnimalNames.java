7a428dcc4ddd7d574db30628df3cd4b7710a1bc6
nayan5565 <nayan5565@gmail.com>
2017-07-31 16:24:14 +0600

    init

---
+++ app/src/main/java/com/nayan/searchviewactivity/AnimalNames.java
@@ -0,0 +1,16 @@
+package com.nayan.searchviewactivity;
+
+/**
+ * Created by Nayan on 7/31/2017.
+ */
+public class AnimalNames {
+    private String animalName;
+
+    public AnimalNames(String animalName) {
+        this.animalName = animalName;
+    }
+
+    public String getAnimalName() {
+        return this.animalName;
+    }
+}
